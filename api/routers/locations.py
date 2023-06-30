56a803d3dadc6cc1a39b70454c3f5bde592f32b5
James Leung <jamesleung7158@gmail.com>
2023-06-29 17:48:34 -0700

    James made small changes

--- api/routers/locations.py
+++ api/routers/locations.py
@@ -5,3 +5,7 @@ router = APIRouter()
 @router.get("/locations")
 def get_locations():
     return {"locations": ["location1"]}
+
+@router.get("/locations/{location_id}")
+def get_location(location_id):
+    return {"location_id": location_id}
